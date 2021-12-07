2b65207c8a485335baf6a666ae74cf1dd2317a0c
Paulo Lima <paulo.lima@zrp.com.br>
2021-12-07 20:29:42 -0300

    fix: removed unecessary comments

--- trem.cpp
+++ trem.cpp
@@ -14,9 +14,9 @@ void Trem::run(){
     while(true){
         switch(ID){
         case 1:     //Trem 1
-            if (y == 30 && x <500) // posição: acima a direita
+            if (y == 30 && x <500)
                 x+=10;
-            else if (x == 500 && y < 150) // posição: abaixo a direita
+            else if (x == 500 && y < 150)
                 y+=10;
             else if (x > 230 && y == 150)
                 x-=10;
@@ -25,9 +25,9 @@ void Trem::run(){
             emit updateGUI(ID, x,y);    //Emite um sinal
             break;
         case 2: //Trem 2
-            if (y == 30 && x <770) // posição: acima a direita
+            if (y == 30 && x <770)
                 x+=10;
-            else if (x == 770 && y < 150) // posição: abaixo a direita
+            else if (x == 770 && y < 150)
                 y+=10;
             else if (x > 500 && y == 150)
                 x-=10;
@@ -36,9 +36,9 @@ void Trem::run(){
             emit updateGUI(ID, x,y);    //Emite um sinal
             break;
         case 3: //Trem 3
-            if (y == 150 && x <370) // posição: acima a direita máxima
+            if (y == 150 && x <370)
                 x+=10;
-            else if (x == 370 && y < 300) // posição: abaixo a direita
+            else if (x == 370 && y < 300)
                 y+=10;
             else if (x > 100 && y == 300)
                 x-=10;
@@ -47,9 +47,9 @@ void Trem::run(){
             emit updateGUI(ID, x,y);    //Emite um sinal
             break;
         case 4: //Trem 4
-            if (y == 150 && x <640) // posição: acima a direita máxima
+            if (y == 150 && x <640)
                 x+=10;
-            else if (x == 640 && y < 300) // posição: abaixo a direita
+            else if (x == 640 && y < 300)
                 y+=10;
             else if (x > 370 && y == 300)
                 x-=10;
@@ -58,9 +58,9 @@ void Trem::run(){
             emit updateGUI(ID, x,y);    //Emite um sinal
             break;
         case 5: //Trem 5
-            if (y == 150 && x <910) // posição: acima a direita máxima
+            if (y == 150 && x <910)
                 x+=10;
-            else if (x == 910 && y < 300) // posição: abaixo a direita
+            else if (x == 910 && y < 300)
                 y+=10;
             else if (x > 640 && y == 300)
                 x-=10;

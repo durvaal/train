1754b7b1ce737ac71888a386d5d7f75df875f4c5
Paulo Lima <paulo.lima@zrp.com.br>
2021-12-20 23:25:46 -0300

    fix: deadlock handle

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -30,23 +30,42 @@ MainWindow::MainWindow(QWidget *parent) :
 }
 
 void MainWindow::checkFirstTrainCanMove(int x, int y) {
-    if ((ui->label_trem1->x() == 460 && ui->label_trem1->y() == 30) && (ui->label_trem2->x() < 600 || (trem4->getCanMove() == TRAIN_CANNOT_MOVE && fourthCriticalRegion.available() == 1)) && firstCriticalRegion.available() == 0)
+    if ((ui->label_trem1->x() == 460 && ui->label_trem1->y() == 30) && (trem4->getCanMove() == TRAIN_CANNOT_MOVE && fourthCriticalRegion.available() == 1) && firstCriticalRegion.available() == 0)
     {
         // AWAIT SECOND TRAIN
         firstCriticalRegion.release(1);
         qDebug() << "RELEASE BY FIRST TRAIN, FIRST CRITICAL REGION AVAILABLE: " << firstCriticalRegion.available();
         trem1->setCanMove(TRAIN_CANNOT_MOVE);
         trem1->wait(1);
-    }
-    else if ((ui->label_trem1->x() == 410 && ui->label_trem1->y() == 150) && (ui->label_trem3->x() > 210 && ui->label_trem3->y() == 150) && secondCriticalRegion.available() == 0)
+    } else if ((ui->label_trem1->x() == 460 && ui->label_trem1->y() == 30) && (ui->label_trem2->x() < 600) && firstCriticalRegion.available() == 0)
+    {
+        // AWAIT THIRD TRAIN
+        firstCriticalRegion.release(1);
+        qDebug() << "RELEASE BY FIRST TRAIN, SECOND CRITICAL REGION AVAILABLE: " << secondCriticalRegion.available();
+        trem1->setCanMove(TRAIN_CANNOT_MOVE);
+        trem1->wait(1);
+    } else if ((ui->label_trem1->x() == 410 && ui->label_trem1->y() == 150) && (ui->label_trem3->x() >= 210 && ui->label_trem3->y() == 150) && secondCriticalRegion.available() == 0)
     {
         // AWAIT THIRD TRAIN
         secondCriticalRegion.release(1);
         qDebug() << "RELEASE BY FIRST TRAIN, SECOND CRITICAL REGION AVAILABLE: " << secondCriticalRegion.available();
         trem1->setCanMove(TRAIN_CANNOT_MOVE);
         trem1->wait(1);
-    }
-    else if ((ui->label_trem1->x() == 500 && ui->label_trem1->y() == 120) && ((ui->label_trem4->x() >= 370 && ui->label_trem4->x() < 540) && ui->label_trem4->y() < 200) && thirdCriticalRegion.available() == 0)
+    } else if ((ui->label_trem1->x() == 500 && ui->label_trem1->y() == 120) && (trem3->getCanMove() == TRAIN_CANNOT_MOVE && sixthCriticalRegion.available() == 1) && thirdCriticalRegion.available() == 0)
+    {
+        // AWAIT FOURTH TRAIN
+        thirdCriticalRegion.release(1);
+        qDebug() << "RELEASE BY FIRST TRAIN, SIXTH CRITICAL REGION AVAILABLE: " << thirdCriticalRegion.available();
+        trem1->setCanMove(TRAIN_CANNOT_MOVE);
+        trem1->wait(1);
+    } else if ((ui->label_trem1->x() == 500 && ui->label_trem1->y() == 120) && ((ui->label_trem3->x() >= 210 && ui->label_trem3->y() == 150) && (ui->label_trem4->x() == 370 && ui->label_trem4->y() <= 300)) && thirdCriticalRegion.available() == 0)
+    {
+        // AWAIT FOURTH TRAIN
+        thirdCriticalRegion.release(1);
+        qDebug() << "RELEASE BY FIRST TRAIN, SIXTH CRITICAL REGION AVAILABLE: " << thirdCriticalRegion.available();
+        trem1->setCanMove(TRAIN_CANNOT_MOVE);
+        trem1->wait(1);
+    } else if ((ui->label_trem1->x() == 500 && ui->label_trem1->y() == 120) && ((ui->label_trem4->x() >= 370 && ui->label_trem4->x() < 540) && ui->label_trem4->y() < 200) && thirdCriticalRegion.available() == 0)
     {
         // AWAIT FOURTH TRAIN
         thirdCriticalRegion.release(1);
@@ -100,19 +119,23 @@ void MainWindow::checkSecondTrainCanMove(int x, int y) {
          qDebug() << "RELEASE BY SECOND TRAIN, FIRST CRITICAL REGION AVAILABLE: " << firstCriticalRegion.available();
          trem2->setCanMove(TRAIN_CANNOT_MOVE);
          trem2->wait(1);
-     } else if ((ui->label_trem2->x() == 690 && ui->label_trem2->y() == 150) && (ui->label_trem4->x() > 390 && ui->label_trem4->x() <= 640 && (ui->label_trem4->y() == 150 || ui->label_trem4->y() < 170)) && fourthCriticalRegion.available() == 0) {
+     } else if ((ui->label_trem2->x() == 660 && ui->label_trem2->y() == 150) && (ui->label_trem4->x() > 390 && ui->label_trem4->x() <= 640 && (ui->label_trem4->y() == 150 || ui->label_trem4->y() < 170)) && fourthCriticalRegion.available() == 0) {
          // AWAIT FOURTH TRAIN
          fourthCriticalRegion.release(1);
          qDebug() << "RELEASE BY SECOND TRAIN, FOURTH CRITICAL REGION AVAILABLE: " << fourthCriticalRegion.available();
          trem2->setCanMove(TRAIN_CANNOT_MOVE);
          trem2->wait(1);
-     } else if ((ui->label_trem2->x() == 770 && ui->label_trem2->y() == 120) && ((ui->label_trem5->x() >= 640 && ui->label_trem5->x() < 800 && (ui->label_trem5->y() == 150 || ui->label_trem5->y() >= 150 && ui->label_trem5->y() <= 200 )) || (trem4->getCanMove() == TRAIN_CANNOT_MOVE && seventhCriticalRegion.available() == 1)) && fifthCriticalRegion.available() == 0) {
+     }
+     else if ((ui->label_trem2->x() == 770 && ui->label_trem2->y() == 130) && ((ui->label_trem5->x() >= 640 && ui->label_trem5->x() < 800 && ui->label_trem5->y() == 150) || (ui->label_trem5->x() < 660 && ui->label_trem4->x() > 480 && ui->label_trem4->y() == 150) || (trem4->getCanMove() == TRAIN_CANNOT_MOVE && seventhCriticalRegion.available() == 1)) && fifthCriticalRegion.available() == 0)
+     {
          // AWAIT FIFTH TRAIN
          fifthCriticalRegion.release(1);
          qDebug() << "RELEASE BY SECOND TRAIN, FIFTH CRITICAL REGION AVAILABLE: " << fifthCriticalRegion.available();
          trem2->setCanMove(TRAIN_CANNOT_MOVE);
          trem2->wait(1);
-     } else {
+     }
+     else
+     {
          if (trem2->getCanMove() == TRAIN_CANNOT_MOVE) {
              if (firstCriticalRegion.available() == 1) {
                  firstCriticalRegion.acquire(1);
@@ -125,7 +148,7 @@ void MainWindow::checkSecondTrainCanMove(int x, int y) {
              if (fourthCriticalRegion.available() == 1) {
                  fourthCriticalRegion.acquire(1);
                  qDebug() << "ACQUIRE BY SECOND TRAIN, FOURTH CRITICAL REGION AVAILABLE: " << fourthCriticalRegion.available();
-                 trem2->setX(690);
+                 trem2->setX(660);
                  trem2->setY(150);
                  trem2->setCanMove(TRAIN_CAN_MOVE);
                  trem2->start();
@@ -134,7 +157,7 @@ void MainWindow::checkSecondTrainCanMove(int x, int y) {
                  fifthCriticalRegion.acquire(1);
                  qDebug() << "ACQUIRE BY SECOND TRAIN, SEVENTH CRITICAL REGION AVAILABLE: " << fifthCriticalRegion.available();
                  trem2->setX(770);
-                 trem2->setY(120);
+                 trem2->setY(130);
                  trem2->setCanMove(TRAIN_CAN_MOVE);
                  trem2->start();
              }
@@ -145,60 +168,68 @@ void MainWindow::checkSecondTrainCanMove(int x, int y) {
 }
 
 void MainWindow::checkThirdTrainCanMove(int x, int y) {
-    if ((ui->label_trem3->x() == 200 && ui->label_trem3->y() == 150) && ((ui->label_trem1->x() < 400 && ui->label_trem1->y() == 150) || (trem4->getCanMove() == TRAIN_CANNOT_MOVE && seventhCriticalRegion.available() == 1)) && thirdCriticalRegion.available() == 0) {
+    if ((ui->label_trem3->x() == 200 && ui->label_trem3->y() == 150) && ((ui->label_trem1->x() <= 400 && ui->label_trem1->y() == 150) || trem4->getCanMove() == TRAIN_CANNOT_MOVE && thirdCriticalRegion.available() == 1) && secondCriticalRegion.available() == 0)
+    {
         // AWAIT FIRST TRAIN
         secondCriticalRegion.release(1);
         qDebug() << "RELEASE BY THIRD TRAIN, SECOND CRITICAL REGION AVAILABLE: " << secondCriticalRegion.available();
         trem3->setCanMove(TRAIN_CANNOT_MOVE);
         trem3->wait(1);
-      } else if ((ui->label_trem3->x() == 340 && ui->label_trem3->y() == 150) && (ui->label_trem4->x() == 370) && sixthCriticalRegion.available() == 0) {
+    }
+    else if ((ui->label_trem3->x() == 330 && ui->label_trem3->y() == 150) && (ui->label_trem4->x() == 370) && sixthCriticalRegion.available() == 0)
+    {
         // AWAIT FOURTH TRAIN
         sixthCriticalRegion.release(1);
         qDebug() << "RELEASE BY THIRD TRAIN, SIXTH CRITICAL REGION AVAILABLE: " << sixthCriticalRegion.available();
         trem3->setCanMove(TRAIN_CANNOT_MOVE);
         trem3->wait(1);
-    } else {
-         if (trem3->getCanMove() == TRAIN_CANNOT_MOVE) {
-             if (secondCriticalRegion.available() == 1) {
-                 secondCriticalRegion.acquire(1);
-                 qDebug() << "ACQUIRE BY THIRD TRAIN, SECOND CRITICAL REGION AVAILABLE: " << secondCriticalRegion.available();
-                 trem3->setX(200);
-                 trem3->setY(150);
-                 trem3->setCanMove(TRAIN_CAN_MOVE);
-                 trem3->start();
-             }
-             if (sixthCriticalRegion.available() == 1) {
-                 sixthCriticalRegion.acquire(1);
-                 qDebug() << "ACQUIRE BY THIRD TRAIN, SIXTH CRITICAL REGION AVAILABLE: " << sixthCriticalRegion.available();
-                 trem3->setX(340);
-                 trem3->setY(150);
-                 trem3->setCanMove(TRAIN_CAN_MOVE);
-                 trem3->start();
-             }
-         } else {
+    }
+    else
+    {
+        if (trem3->getCanMove() == TRAIN_CANNOT_MOVE)
+        {
+            if (secondCriticalRegion.available() == 1)
+            {
+                secondCriticalRegion.acquire(1);
+                qDebug() << "ACQUIRE BY THIRD TRAIN, SECOND CRITICAL REGION AVAILABLE: " << secondCriticalRegion.available();
+                trem3->setX(200);
+                trem3->setY(150);
+                trem3->setCanMove(TRAIN_CAN_MOVE);
+                trem3->start();
+            }
+            if (sixthCriticalRegion.available() == 1)
+            {
+                sixthCriticalRegion.acquire(1);
+                qDebug() << "ACQUIRE BY THIRD TRAIN, SIXTH CRITICAL REGION AVAILABLE: " << sixthCriticalRegion.available();
+                trem3->setX(330);
+                trem3->setY(150);
+                trem3->setCanMove(TRAIN_CAN_MOVE);
+                trem3->start();
+            }
+        }
+        else
+        {
             ui->label_trem3->setGeometry(x,y,21,17);
          }
-     }
+    }
 }
 
 void MainWindow::checkFourthTrainCanMove(int x, int y) {
-    if ((ui->label_trem4->x() == 370 && ui->label_trem4->y() == 180) && (((ui->label_trem1->x() > 340 && ui->label_trem1->x() <= 500) && (ui->label_trem1->y() == 120 || ui->label_trem1->y() > 120)) || (trem2->getCanMove() == TRAIN_CANNOT_MOVE && firstCriticalRegion.available() == 1)) && thirdCriticalRegion.available() == 0)
-    {
+    if ((ui->label_trem4->x() == 370 && ui->label_trem4->y() == 180) && ((((ui->label_trem1->x() > 340 && ui->label_trem1->x() <= 500) && (ui->label_trem1->y() == 120)) || (ui->label_trem1->x() <= 500) && (ui->label_trem1->y() >= 120)) && thirdCriticalRegion.available() == 0)) {
         // AWAIT FIRST TRAIN
         thirdCriticalRegion.release(1);
         qDebug() << "RELEASE BY FOURTH TRAIN, THIRD CRITICAL REGION AVAILABLE: " << thirdCriticalRegion.available();
         trem4->setCanMove(TRAIN_CANNOT_MOVE);
         trem4->wait(1);
     }
-    else if ((ui->label_trem4->x() == 390 && ui->label_trem4->y() == 300) && (ui->label_trem3->x() == 370 && ui->label_trem3->y() >= 150) && sixthCriticalRegion.available() == 0)
-    {
+    else if ((ui->label_trem4->x() == 400 && ui->label_trem4->y() == 300) && (((ui->label_trem1->x() <= 500) && (ui->label_trem1->y() == 150)) && ((ui->label_trem3->x() >= 120) && (ui->label_trem1->y() == 150))) && sixthCriticalRegion.available() == 0) {
         // AWAIT THIRD TRAIN
         sixthCriticalRegion.release(1);
         qDebug() << "RELEASE BY FOURTH TRAIN, SIXTH CRITICAL REGION AVAILABLE: " << sixthCriticalRegion.available();
         trem4->setCanMove(TRAIN_CANNOT_MOVE);
         trem4->wait(1);
     }
-    else if ((ui->label_trem4->x() == 460 && ui->label_trem4->y() == 150) && (ui->label_trem2->x() <= 640 && ui->label_trem2->y() == 150) && fourthCriticalRegion.available() == 0)
+    else if ((ui->label_trem4->x() == 480 && ui->label_trem4->y() == 150) && ((ui->label_trem2->x() < 660 && ui->label_trem2->y() == 150) || (ui->label_trem2->x() <= 770 && ui->label_trem2->y() == 150 && ui->label_trem5->x() < 660) || (trem2->getCanMove() == TRAIN_CANNOT_MOVE && fifthCriticalRegion.available() == 1)) && fourthCriticalRegion.available() == 0)
     {
         // AWAIT SECOND TRAIN
         fourthCriticalRegion.release(1);
@@ -206,7 +237,15 @@ void MainWindow::checkFourthTrainCanMove(int x, int y) {
         trem4->setCanMove(TRAIN_CANNOT_MOVE);
         trem4->wait(1);
     }
-    else if ((ui->label_trem4->x() == 620 && ui->label_trem4->y() == 150) && (ui->label_trem5->x() == 640) && seventhCriticalRegion.available() == 0)
+    else if ((ui->label_trem4->x() == 400 && ui->label_trem4->y() == 300) && (ui->label_trem3->x() == 370 && ui->label_trem3->y() >= 150) && sixthCriticalRegion.available() == 0)
+    {
+        // AWAIT THIRD TRAIN
+        sixthCriticalRegion.release(1);
+        qDebug() << "RELEASE BY FOURTH TRAIN, SIXTH CRITICAL REGION AVAILABLE: " << sixthCriticalRegion.available();
+        trem4->setCanMove(TRAIN_CANNOT_MOVE);
+        trem4->wait(1);
+    }
+    else if ((ui->label_trem4->x() == 620 && ui->label_trem4->y() == 150) && (ui->label_trem5->x() <= 660) && seventhCriticalRegion.available() == 0)
     {
         // AWAIT FIFTH TRAIN
         seventhCriticalRegion.release(1);
@@ -223,7 +262,7 @@ void MainWindow::checkFourthTrainCanMove(int x, int y) {
                 thirdCriticalRegion.acquire(1);
                 qDebug() << "ACQUIRE BY FOURTH TRAIN, THIRD CRITICAL REGION AVAILABLE: " << thirdCriticalRegion.available();
                 trem4->setX(370);
-                trem4->setY(170);
+                trem4->setY(180);
                 trem4->setCanMove(TRAIN_CAN_MOVE);
                 trem4->start();
             }
@@ -231,7 +270,7 @@ void MainWindow::checkFourthTrainCanMove(int x, int y) {
             {
                 sixthCriticalRegion.acquire(1);
                 qDebug() << "ACQUIRE BY FOURTH TRAIN, SIXTH CRITICAL REGION AVAILABLE: " << sixthCriticalRegion.available();
-                trem4->setX(390);
+                trem4->setX(400);
                 trem4->setY(300);
                 trem4->setCanMove(TRAIN_CAN_MOVE);
                 trem4->start();
@@ -269,41 +308,43 @@ void MainWindow::checkFifthTrainCanMove(int x, int y) {
         qDebug() << "RELEASE BY FIFTH TRAIN, FIFTH CRITICAL REGION AVAILABLE: " << fifthCriticalRegion.available();
         trem5->setCanMove(TRAIN_CANNOT_MOVE);
         trem5->wait(1);
-     } else if ((ui->label_trem5->x() == 660 && ui->label_trem5->y() == 300) && (ui->label_trem4->x() == 640 && ui->label_trem4->y() >= 150) && seventhCriticalRegion.available() == 0) {
+     }
+    else if ((ui->label_trem5->x() == 660 && ui->label_trem5->y() == 300) && ((ui->label_trem4->x() > 620) || (trem2->getCanMove() == TRAIN_CANNOT_MOVE && fourthCriticalRegion.available() == 1) || (ui->label_trem4->x() > 480 && ui->label_trem4->y() == 150 && ui->label_trem2->x() <= 770 && ui->label_trem2->y() == 150)) && seventhCriticalRegion.available() == 0)
+    {
         // AWAIT FOURTH TRAIN
         seventhCriticalRegion.release(1);
         qDebug() << "RELEASE BY FIFTH TRAIN, SEVENTH CRITICAL REGION AVAILABLE: " << seventhCriticalRegion.available();
         trem5->setCanMove(TRAIN_CANNOT_MOVE);
         trem5->wait(1);
-    } else if ((ui->label_trem5->x() == 660 && ui->label_trem5->y() == 300) && (ui->label_trem2->x() == 660 && ui->label_trem2->y() == 150) && seventhCriticalRegion.available() == 0) {
-        // AWAIT SECOND TRAIN WHENT SECOND TRAIN AWAIT FOURTH TRAIN
-        seventhCriticalRegion.release(1);
-        qDebug() << "RELEASE BY FIFTH TRAIN, SEVENTH CRITICAL REGION AVAILABLE: " << seventhCriticalRegion.available();
-        trem5->setCanMove(TRAIN_CANNOT_MOVE);
-        trem5->wait(1);
-     } else {
-         if (trem5->getCanMove() == TRAIN_CANNOT_MOVE) {
-             if (fifthCriticalRegion.available() == 1) {
-                 fifthCriticalRegion.acquire(1);
-                 qDebug() << "ACQUIRE BY FIFTH TRAIN, FIFTH CRITICAL REGION AVAILABLE: " << fifthCriticalRegion.available();
-                 trem5->setX(640);
-                 trem5->setY(170);
-                 trem5->setCanMove(TRAIN_CAN_MOVE);
-                 trem5->start();
-             }
-             if (seventhCriticalRegion.available() >= 1) {
-                 seventhCriticalRegion.acquire(1);
-                 qDebug() << "ACQUIRE BY FIFTH TRAIN, SEVENTH CRITICAL REGION AVAILABLE: " << seventhCriticalRegion.available();
-                 trem5->setX(660);
-                 trem5->setY(300);
-                 trem5->setCanMove(TRAIN_CAN_MOVE);
-                 trem5->start();
-             }
-         } else {
-            ui->label_trem5->setGeometry(x,y,21,17);
-         }
-     }
-}
+    } else
+        {
+            if (trem5->getCanMove() == TRAIN_CANNOT_MOVE)
+            {
+                if (fifthCriticalRegion.available() == 1)
+                {
+                    fifthCriticalRegion.acquire(1);
+                    qDebug() << "ACQUIRE BY FIFTH TRAIN, FIFTH CRITICAL REGION AVAILABLE: " << fifthCriticalRegion.available();
+                    trem5->setX(640);
+                    trem5->setY(170);
+                    trem5->setCanMove(TRAIN_CAN_MOVE);
+                    trem5->start();
+                }
+                if (seventhCriticalRegion.available() >= 1)
+                {
+                    seventhCriticalRegion.acquire(1);
+                    qDebug() << "ACQUIRE BY FIFTH TRAIN, SEVENTH CRITICAL REGION AVAILABLE: " << seventhCriticalRegion.available();
+                    trem5->setX(660);
+                    trem5->setY(300);
+                    trem5->setCanMove(TRAIN_CAN_MOVE);
+                    trem5->start();
+                }
+            }
+            else
+            {
+                ui->label_trem5->setGeometry(x, y, 21, 17);
+            }
+        }
+    }
 
 //Função que será executada quando o sinal UPDATEGUI for emitido
 void MainWindow::updateInterface(int id, int x, int y){
